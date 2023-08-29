023a2861828c25817e1bcebdeb891e7279ef911c
LOGESHKUMAR.N <106069063+180701127@users.noreply.github.com>
2023-08-29 09:38:46 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent},
+  {path: 'welcome/:userName',component:WelcomeComponent},
+  {path: '**',redirectTo:''},//Redirect any route to the login page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
